f8fcee194eb0071721d215c254a2517b2a535e7f
Brady Lamson <lamsonbrady@gmail.com>
2022-02-19 10:58:42 -0700

    cleaning data frame

--- r_code/testing_and_plotting.R
+++ r_code/testing_and_plotting.R
@@ -1,6 +1,8 @@
 # load in new updated data frames
-bitcoin_df <- read.csv("data_frames/bitcoin_df")
-gold_df <- read.csv("data_frames/gold_df")
+bitcoin_df <- read.csv("BCHAIN-MKPRU.csv") %>%
+    as_tibble()
+gold_df <- read.csv("LBMA-GOLD.csv") %>%
+    as_tibble()
 
 library(dplyr)
 library(ggplot2)
@@ -15,14 +17,14 @@ calc_vol <- function(df, log = FALSE) {
         diff_vec <- df$differences
         
         for (i in 1:length(value_vec)) {
-            new_val <- c(sd(c(diff_vec[i+1], diff_vec[i])))
+            new_val <- c(sd(c(diff_vec[i], diff_vec[i-1])))
             ret_val <- c(ret_val, new_val)
         }
     }
     else {
         log_diff_vec <- df$log_differences
         for (i in 1:length(log_diff_vec)) {
-            new_val <- c(sd(c(log_diff_vec[i+1], log_diff_vec[i])))
+            new_val <- c(sd(c(log_diff_vec[i], log_diff_vec[i-1])))
             ret_val <- c(ret_val, new_val)
         }
     }
@@ -33,31 +35,38 @@ calc_vol <- function(df, log = FALSE) {
 # Using mutate to add differences and log differences
 # in both the bitcoin and gold data frames
 
-# Calculate vol for bitcoin
+# Mutate bitcoin df ----------------------------
+bitcoin_df <- bitcoin_df %>%
+    dplyr::mutate(
+        differences = (Value - shift(Value, n = 1)) / Value * 100,
+        log_differences = log(Value / shift(Value, n = 1)),
+    )
+    
 btc_vol <- calc_vol(bitcoin_df)
 btc_log_vol <- calc_vol(bitcoin_df, log = TRUE)
 
 bitcoin_df %>%
     dplyr::mutate(
-        differences = (Value - shift(Value, n = 1)) / Value * 100,
-        log_differences = log(Value / shift(Value, n = 1)),
         volatility = btc_vol,
         log_volatility = btc_log_vol
     ) %>%
     write.csv(file = "bitcoin_df")
     #View()
 
-# Calculate vol for gold
-gold_vol <- calc_vol(gold_df)
-gold_log_vol <- calc_vol(gold_df, log = TRUE)
-
-# Modify data frame
-gold_df %>%
+# Mutate gold df -------------------------------
+gold_df <- gold_df %>%
     dplyr::mutate(
         differences = (USD..PM. - shift(USD..PM., n = 1)) / USD..PM. * 100,
         log_differences = log(USD..PM. / shift(USD..PM., n = 1)),
-        voltaility = gold_vol,
+    )
+
+gold_vol <- calc_vol(gold_df)
+gold_log_vol <- calc_vol(gold_df, log = TRUE)
+
+gold_df %>%
+    dplyr::mutate(
+        volatility = gold_vol,
         log_volatility = gold_log_vol
     ) %>%
-    #write.csv(file = "gold_df")
-    View()
+    write.csv(file = "gold_df")
+#View()
