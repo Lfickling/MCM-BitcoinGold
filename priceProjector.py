b1d63e474d161096878c35c472661d88408354d2
Lfickling <lettiefickling@gmail.com>
2022-02-19 15:56:25 -0700

    change

--- priceProjector.py
+++ priceProjector.py
@@ -10,45 +10,51 @@ def trader_main():
     comparedReturns = {"date":[],"real":[], "projected":[]}
     cumVolatility = 0
     sigmas =  []
+    yesterdayPrice = 621
 
     bitcoinDF = pd.read_csv('/home/lfickling/Spring 22/MCM/MCM-BitcoinGold/data_frames/bitcoin_df.csv')
 
-    for i in range(0,15):
+    for i in range(0,10):
         row = bitcoinDF.loc[i].to_list()
         sigma = row[5]
         
-        if i >= 3:
-            cumVolatility += (sigma ** 2)
-            newSigma = (sqrt((1/(i-2)) * cumVolatility))/(i-1)
-            sigmas.append(newSigma)
-            sigma = newSigma
+        if i >= 2:
+            #cumVolatility += (sigma ** 2)
+            #newSigma = (sqrt((1/(i-2)) * cumVolatility))
+            #sigmas.append(newSigma)
+            #sigma = newSigma
+            sigma = row[9]
         else:
             sigma = 0
+        print("****** ", i)
 
-        RowReturner = ProjReturn(sigma, row[8], row[2]) #volatility, cumulative mu, value(price)
+        RowReturner = ProjReturn(sigma, row[8], yesterdayPrice) #volatility, cumulative mu, value(price)
         projectedReturn = RowReturner.getReturn()
         comparedReturns["date"].append(row[1])
         comparedReturns["real"].append(row[2])
-        comparedReturns["projected"].append(projectedReturn)       
+        comparedReturns["projected"].append(projectedReturn)  
+
+        yesterdayPrice = row[2]     
     
     comparedReturnsDF = pd.DataFrame(comparedReturns)
 
     print(comparedReturnsDF)
-
-    #ax = plt.gca() 
-    #comparedReturnsDF.plot(kind = 'line',
-            #x = 'date',
-            #y = 'real',
-            #color = 'green',ax = ax)
-    #comparedReturnsDF.plot(kind = 'line',x = 'name',
-     #       y = 'projected',
-      #      color = 'blue',ax = ax)
+"""
+    ax = plt.gca() 
+    comparedReturnsDF.plot(kind = 'line',
+            x = 'date',
+            y = 'real',
+            color = 'green',ax = ax)
+    comparedReturnsDF.plot(kind = 'line',x = 'date',
+            y = 'projected',
+            color = 'blue',ax = ax)
       
     # set the title
-    #plt.title('LinePlots')
+    plt.title('LinePlots')
     
     # show the plot
-    #plt.show()
+    plt.show()
+    """
 
 if __name__ == '__main__':
     trader_main()
--- projReturn.py
+++ projReturn.py
@@ -1,7 +1,7 @@
 from tokenize import Double
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+from math import e
 
 np.random.seed(0)
 
@@ -11,19 +11,22 @@ class ProjReturn():
     mu = 0
     averageReturn = 0
 
-    def __init__(self,sigma=0.0, mu=0.0, price = 0.0, k = 10000):
+    def __init__(self,sigma=0.0, mu=0.0, price = 0.0, k = 1):
         self.sigma = sigma
         self.mu = mu
         sumReturns = 0
-        for i in range(k-1):
+        for i in range(k):
             sumReturns += self.getReturnInstance(price)
         self.averageReturn = sumReturns / k
-        #print(self.mu + " " + self.sigma + " " + price + " " + self.averageReturn)
-
+        print("mu is ", self.mu)
+        print("sigma is ", self.sigma)
+        print("price is ", price)
 
     def getReturnInstance(self, price):
-        norm = np.random.normal(loc = self.mu, scale = self.sigma)
-        returnInstance = price * math.exp(-1 * (self.mu - (0.5 * (self.sigma**2))) * (self.sigma * norm))  
+        norm = np.random.normal(0, 1)
+        returnInstance = price * (e ** (-1 * (self.mu - (0.5 * (self.sigma**2))) * (self.sigma * norm)))  
+        print("scalar is ", (returnInstance / price))
+        print("exponent is ", (-1 * (self.mu - (0.5 * (self.sigma**2))) * (self.sigma * norm)))
         return returnInstance
         
     def getReturn(self):
