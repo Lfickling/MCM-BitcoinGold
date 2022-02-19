dc0e1ad8deb8299c9d78d75184fb573a64ca5447
Lfickling <lettiefickling@gmail.com>
2022-02-19 11:56:32 -0700

    some random things

--- FormatData.py
+++ FormatData.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import sys
 import datetime
 #from sklearn.linear_model import LinearRegression
-#import financialanalysis as fa
+import financialanalysis as fa
 
 
 goldData = pd.read_csv("./LBMA-GOLD.csv", parse_dates=['Date'], index_col='Date')
@@ -18,33 +18,7 @@ goldData = pd.read_csv("./LBMA-GOLD.csv", parse_dates=['Date'], index_col='Date'
 goldData["datetime"] = fa.stringToDatetime(goldData["Date"].to_list())
 
 
-## Prepare data
-X = goldData["datetime"].to_list() # convert Series to list
-#X = fa.datetimeToFloatyear(X) # for example, 2020-07-01 becomes 2020.49589041
-#print(X) # [2020.0054794520547, 2020.0082191780823, 2020.01643835616, ...]
-#X = np.array(X) # convert list to a numpy array
-#X -= X[0] # make it begin with 0.
-#print(X) # [0., 0.00273973, 0.0109589, ..., 1.29589041]
-#X = X[::,None] # convert row vector to column vector (just column vector is acceptable)
 
-y = goldData["Value"] # get y data (relative price)
-#y = y.values # convert Series to numpy
-#y = y[::,None] # row vector to column vector (just column vector is acceptable)
-
-priceReturns = {}
-for i in range(len(X)):
-    priceReturnToDate = 0
-    currentPrice = y[i]
-    for j in range(i):
-        priceReturnToDate += log(currentPrice - y[j])
-    priceReturns[X[i]] = priceReturnToDate / i
-
-historicReturns = pd.DataFrame.from_dict(priceReturns, orient='index')
-
-
-plt.figure()
-
-historicReturns.plot()
 
 
 
--- trader.py
+++ trader.py
@@ -1,3 +1,9 @@
-class bitcoin:
-    def __init__(self) -> None:
-        pass
+#need program to forecast expected return
+#four models running over time (3?)
+#1)risk free return of each asset
+#2)calculating realized volatility
+#3) risk premium
+#4)return of the asset to date (price of the asset on that date vs day 1
+#
+
+
