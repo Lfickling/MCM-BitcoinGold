9403bab52916329a3570602c57645aca270565a2
Brady Lamson <lamsonbrady@gmail.com>
2022-02-21 16:34:48 -0700

    final batch of plotting code.

--- r_code/plotting.R
+++ r_code/plotting.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 bitcoin_df <- read.csv("data_frames/bitcoin_df.csv")
 gold_df <- read.csv("data_frames/gold_df.csv")
+new_btc_df <- read.csv("data_frames/bitcoin_dfnew.csv")
+new_gold_df <- read.csv("data_frames/gold_dfnew.csv")
 
 # ------------------[ LINE PLOTS ]----------------------
 # -------[ BITCOIN PLOTS ]------------ 
@@ -54,6 +56,32 @@ ggplot2::ggplot() +
         axis.title.x = element_blank(),
     )
 
+# Line plots for mu and sigma
+# BTC ------
+ggplot2::ggplot() +
+    geom_line(data = new_btc_df, aes(x = as.Date(Date), y = mu)) +
+    #ylim(c(-0.3,0.2)) +
+    scale_x_date(date_labels = "%d %b %Y") +
+    labs(
+        title = "Mu of Bitcoin Data Set Over Time") +
+    ylab("Value of Mu") +
+    theme(
+        axis.title.x = element_blank()
+    ) 
+
+# Gold -----
+ggplot2::ggplot() +
+    geom_line(data = new_gold_df, aes(x = as.Date(Date), y = mu)) +
+    #ylim(c(-0.3,0.2)) +
+    scale_x_date(date_labels = "%d %b %Y") +
+    labs(
+        title = "Mu of Gold Data Set Over Time") +
+    ylab("Value of Mu") +
+    theme(
+        axis.title.x = element_blank()
+    ) 
+
+
 # --------------[ HISTOGRAMS ]-----------------------
 # Bitcoin log return histogram
 ggplot2::ggplot(data = bitcoin_df) +
@@ -83,16 +111,17 @@ ggplot2::ggplot(data = gold_df) +
     ) +
     xlab("Log Return") +
     ylab("Frequency") +
+    xlim(c(-0.06, 0.06)) +
     theme_gray()
 
 # Gold true return histogram
 ggplot2::ggplot(data = gold_df) +
     geom_histogram(aes(x = differences)) +
     labs(
-        title = "True Returns of Bitcoin"
+        title = "True Returns of Gold"
     ) +
     xlab("Return (%)") +
-    ylab("Frequency") +
+    ylab("Frequency")
     theme_gray()
 
 # --------------[ DENSITY PLOT ]-----------------------
@@ -107,8 +136,22 @@ ggplot2::ggplot(data = bitcoin_df, mapping = aes(x = differences)) +
         breaks = c(-3:3 * sd(bitcoin_df$differences, na.rm = TRUE) + mean(bitcoin_df$differences, na.rm = TRUE)),
         labels = c("\u03BC - 3\u03C3", "\u03BC - 2\u03C3", "\u03BC - \u03C3", "\u03BC", "\u03BC + \u03C3", "\u03BC + 2\u03C3", "\u03BC + 3\u03C3")
     ) +
-    theme(axis.text.x = element_text(size=12),
+    theme(axis.text.x = element_text(size=11),
           axis.title.y = element_blank())
+
+# Bitcoin Density plot w/ tail -----
+ggplot2::ggplot(data = bitcoin_df, mapping = aes(x = differences)) +
+    ggplot2::geom_density() +
+    xlab("Returns (%)") +
+    labs(title = "Probability Distribution of Bitcoin Returns") +
+    scale_x_continuous(
+        
+        breaks = c(c(-3, -1, 0, 1, 3) * sd(bitcoin_df$differences, na.rm = TRUE) + mean(bitcoin_df$differences, na.rm = TRUE)),
+        labels = c("\u03BC - 3\u03C3", "\u03BC - \u03C3", "\u03BC", "\u03BC + \u03C3", "\u03BC + 3\u03C3")
+    ) +
+    theme(axis.text.x = element_text(size=11),
+          axis.title.y = element_blank())
+
 
 # Gold Density plot -----
 ggplot2::ggplot(data = gold_df, mapping = aes(x = differences)) +
@@ -116,9 +159,34 @@ ggplot2::ggplot(data = gold_df, mapping = aes(x = differences)) +
     xlab("Returns (%)") +
     labs(title = "Probability Distribution of Gold Returns") +
     scale_x_continuous(
-        limits = c(-2.5,2.5),
+        limits = c(-5,5),
         breaks = c(-3:3 * sd(gold_df$differences, na.rm = TRUE) + mean(gold_df$differences, na.rm = TRUE)),
         labels = c("\u03BC - 3\u03C3", "\u03BC - 2\u03C3", "\u03BC - \u03C3", "\u03BC", "\u03BC + \u03C3", "\u03BC + 2\u03C3", "\u03BC + 3\u03C3")
+    ) +
+    theme(axis.text.x = element_text(size=11),
+          axis.title.y = element_blank())
+
+
+
+
+
+# Gold Density plot w/ tail -----
+ggplot2::ggplot(data = gold_df, mapping = aes(x = differences)) +
+    ggplot2::geom_density() +
+    xlab("Returns (%)") +
+    labs(title = "Probability Distribution of Gold Returns") +
+    xlim(c(-15,15))
+
+# Gold Density plot w/ tail -----
+# Bitcoin Density plot w/ tail -----
+ggplot2::ggplot(data = bitcoin_df, mapping = aes(x = differences)) +
+    ggplot2::geom_density() +
+    xlab("Returns (%)") +
+    labs(title = "Probability Distribution of Gold Returns") +
+    scale_x_continuous(
+        limits = c(-6,6),
+        breaks = c(c(-3, -1, 0, 1, 3) * sd(gold_df$differences, na.rm = TRUE) + mean(gold_df$differences, na.rm = TRUE)),
+        labels = c("\u03BC - 3\u03C3", "\u03BC - \u03C3", "\u03BC", "\u03BC + \u03C3", "\u03BC + 3\u03C3")
     ) +
     theme(axis.text.x = element_text(size=12),
           axis.title.y = element_blank())
